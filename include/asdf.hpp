a980b4c5c0f2c455d4d236270e7e6c7ca01fdd10
Daniel D'Avella <drdavella@gmail.com>
2017-12-22 12:34:42 -0500

    Parse basic YAML file with ASDF header comments

--- include/asdf.hpp
+++ include/asdf.hpp
@@ -1,6 +1,7 @@
+#pragma once
+
 #include <cstdio>
 #include <cstdint>
-#include <yaml-cpp/yaml.h>
 
 
 int load_asdf_file(const char *filename);
--- src/asdf.cpp
+++ src/asdf.cpp
@@ -1,14 +1,49 @@
+#include <iostream>
+#include <fstream>
 #include <cstdio>
+#include <cstring>
 #include <yaml-cpp/yaml.h>
 #include <asdf.hpp>
 
+#define ASDF_HEADER             "#ASDF"
+#define ASDF_STANDARD_HEADER    "#ASDF_STANDARD"
 
+
+static bool parse_header(std::ifstream &ifs)
+{
+    std::string line;
+    std::getline(ifs, line);
+
+    if (line.compare(0, strlen(ASDF_HEADER), ASDF_HEADER) != 0)
+    {
+        return false;
+    }
+
+    std::getline(ifs, line);
+    if (line.compare(0, strlen(ASDF_STANDARD_HEADER), ASDF_STANDARD_HEADER) !=0)
+    {
+        return false;
+    }
+
+    return true;
+}
 
 int load_asdf_file(const char *filename)
 {
     printf("loading %s\n", filename);
 
-    YAML::Node asdf = YAML::LoadFile(filename);
+    std::ifstream ifs;
+    ifs.open(filename);
+
+    if (!parse_header(ifs))
+    {
+        fprintf(stderr, "Invalid ASDF header\n");
+        return 1;
+    }
+
+    YAML::Node asdf = YAML::Load(ifs);
+    std::cout << asdf["top"] << std::endl;
+
 
     return 0;
 }
