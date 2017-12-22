f35fb076e5724606f6b44938d047d18f8998f154
Daniel D'Avella <drdavella@gmail.com>
2017-12-22 13:16:01 -0500

    Locate end of YAML metadata

--- src/asdf.cpp
+++ src/asdf.cpp
@@ -7,6 +7,7 @@
 
 #define ASDF_HEADER             "#ASDF"
 #define ASDF_STANDARD_HEADER    "#ASDF_STANDARD"
+#define YAML_END_MARKER         "..."
 
 
 static bool parse_header(std::ifstream &ifs)
@@ -14,18 +15,34 @@ static bool parse_header(std::ifstream &ifs)
     std::string line;
     std::getline(ifs, line);
 
-    if (line.compare(0, strlen(ASDF_HEADER), ASDF_HEADER) != 0)
+    if (line.compare(0, strlen(ASDF_HEADER), ASDF_HEADER))
     {
         return false;
     }
 
     std::getline(ifs, line);
-    if (line.compare(0, strlen(ASDF_STANDARD_HEADER), ASDF_STANDARD_HEADER) !=0)
+    if (line.compare(0, strlen(ASDF_STANDARD_HEADER), ASDF_STANDARD_HEADER))
     {
         return false;
     }
 
     return true;
+}
+
+static std::streampos find_yaml_end(std::ifstream &ifs)
+{
+    std::string line;
+
+    while(std::getline(ifs, line))
+    {
+        std::cout << line << std::endl;
+        if (line.compare(YAML_END_MARKER) == 0)
+        {
+            break;
+        }
+    }
+
+    return ifs.tellg();
 }
 
 int load_asdf_file(const char *filename)
@@ -40,6 +57,12 @@ int load_asdf_file(const char *filename)
         fprintf(stderr, "Invalid ASDF header\n");
         return 1;
     }
+
+    std::streampos end_index = find_yaml_end(ifs);
+    std::cout << "end index=" << end_index << std::endl;
+
+    /* Reset stream to the beginning of the file */
+    ifs.seekg(0);
 
     YAML::Node asdf = YAML::Load(ifs);
     std::cout << asdf["top"] << std::endl;
