7374163b1ff5f45f468715cb3d0eabbe08fbbd60
Daniel D'Avella <drdavella@gmail.com>
2018-05-10 10:36:20 -0400

    Adjust now that nodebuilder is part of yaml-cpp public API

--- src/nodebuilder.h
+++ src/nodebuilder.h
@@ -6,6 +6,7 @@
 #include "yaml-cpp/emitterstyle.h"
 #include "yaml-cpp/eventhandler.h"
 #include "yaml-cpp/node/ptr.h"
+#include "yaml-cpp/nodebuilder.h"
 
 #include <node.hpp>
 
@@ -23,45 +24,8 @@ using YAML::Mark;
 using YAML::anchor_t;
 using YAML::EmitterStyle;
 
-class NodeBuilder : public YAML::EventHandler {
- public:
-  NodeBuilder();
-  virtual ~NodeBuilder();
-
-  Node Root();
-
-  virtual void OnDocumentStart(const Mark& mark);
-  virtual void OnDocumentEnd();
-
-  virtual void OnNull(const Mark& mark, anchor_t anchor);
-  virtual void OnAlias(const Mark& mark, anchor_t anchor);
-  virtual void OnScalar(const Mark& mark, const std::string& tag,
-                        anchor_t anchor, const std::string& value);
-
-  virtual void OnSequenceStart(const Mark& mark, const std::string& tag,
-                               anchor_t anchor, EmitterStyle::value style);
-  virtual void OnSequenceEnd();
-
-  virtual void OnMapStart(const Mark& mark, const std::string& tag,
-                          anchor_t anchor, EmitterStyle::value style);
-  virtual void OnMapEnd();
-
- private:
-  YAML::detail::node& Push(const Mark& mark, anchor_t anchor);
-  void Push(YAML::detail::node& node);
-  void Pop();
-  void RegisterAnchor(anchor_t anchor, YAML::detail::node& node);
-
- private:
-  YAML::detail::shared_memory_holder m_pMemory;
-  YAML::detail::node* m_pRoot;
-
-  typedef std::vector<YAML::detail::node*> Nodes;
-  Nodes m_stack;
-  Nodes m_anchors;
-
-  typedef std::pair<YAML::detail::node*, bool> PushedKey;
-  std::vector<PushedKey> m_keys;
-  std::size_t m_mapDepth;
+class NodeBuilder : public YAML::NodeBuilder {
+    public:
+        Node Root();
 };
 }
