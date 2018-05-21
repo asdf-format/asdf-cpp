39ad3df848fbe640fc46f0b30700c344afc0eb0b
Daniel D'Avella <drdavella@gmail.com>
2018-05-21 18:35:18 -0400

    Remove pseudo-indexing operator from NDArray

--- include/tags/ndarray.hpp
+++ include/tags/ndarray.hpp
@@ -41,23 +41,6 @@ class NDArray : public AbstractNDArray
             return (T *) file->get_block(source);
         }
 
-        template<typename... Longs>
-        T operator()(Longs... indices)
-        {
-            std::vector<long> values = {indices...};
-            if (values.size() != shape.size())
-            {
-                std::stringstream msg;
-                msg << "Given number of array indices (" << values.size();
-                msg << ") doesn't match dimensions (" << shape.size();
-                msg << ")";
-
-                throw std::runtime_error(msg.str());
-            }
-
-            return 1;
-        }
-
     protected:
         friend class Node;
         friend struct YAML::convert<Asdf::NDArray<T>>;
