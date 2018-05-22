cfc562697d02facd76fcc08bee52491b1770f173
Daniel D'Avella <ddavella@stsci.edu>
2018-05-22 16:17:55 -0400

    Only define dtype for long if using llvm

--- include/private/datatypes.hpp
+++ include/private/datatypes.hpp
@@ -24,7 +24,6 @@ template <typename T> std::string dtype_to_string(void);
 template <typename T> bool dtype_matches(std::string string);
 
 DECLARE_DTYPE_FUNCTIONS(char,       "int8");
-DECLARE_DTYPE_FUNCTIONS(long,       "int64");
 
 DECLARE_DTYPE_FUNCTIONS(int8_t,     "int8");
 DECLARE_DTYPE_FUNCTIONS(int16_t,    "int16");
@@ -41,3 +40,8 @@ static_assert(CHAR_BIT * sizeof(float) == 32, "Unexpected size for float datatyp
 static_assert(CHAR_BIT * sizeof(double) == 64, "Unexpected size for double datatype");
 DECLARE_DTYPE_FUNCTIONS(float,      "float32");
 DECLARE_DTYPE_FUNCTIONS(double,     "float64");
+
+#ifdef __clang__
+static_assert(CHAR_BIT * sizeof(long) == 64, "Unexpected size for long datatype");
+DECLARE_DTYPE_FUNCTIONS(long,       "int64");
+#endif
