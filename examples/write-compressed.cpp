40c94c6f49cc57d61adef464062ff750bbcbe14e
Daniel D'Avella <ddavella@stsci.edu>
2018-05-24 10:17:21 -0400

    Add example program for writing compressed data

---
+++ examples/write-compressed.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+#include <asdf-cpp/asdf.hpp>
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: %s <file name>\n", argv[0]);
+        return 1;
+    }
+
+    Asdf::AsdfFile asdf;
+    Asdf::Node tree = asdf.get_tree();
+
+    /* Create 1D array of integers */
+    int array_size = 1000;
+    int *nums = (int *) calloc(1, sizeof(*nums) * array_size);
+    for (int i = 0; i < array_size; i++)
+    {
+        nums[i] = i;
+    }
+
+    tree["array"] = Asdf::NDArray<int>(nums, array_size);
+
+    std::ofstream outfile(argv[1]);
+    outfile << asdf;
+}
