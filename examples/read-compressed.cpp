6fea0672454a1e4fb1c603b72345d912fdfdadbd
Daniel D'Avella <drdavella@gmail.com>
2018-05-23 20:53:15 -0400

    Add example program to read files with compressed blocks

---
+++ examples/read-compressed.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+
+#include <asdf-cpp/asdf.hpp>
+
+
+using namespace Asdf;
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: %s <file name>\n", argv[0]);
+        return 1;
+    }
+
+    Asdf::AsdfFile asdf(argv[1]);
+    Asdf::Node tree = asdf.get_tree();
+
+    auto data_array = tree["data"].as<NDArray<int64_t>>();
+    auto data = data_array.read().get();
+
+    for (size_t i = 0; i < data_array.get_shape()[0]; i++)
+    {
+        assert(data[i] == i);
+    }
+}
