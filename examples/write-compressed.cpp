cb3193272c334089f71e16c4c8a58ef80d453a7f
Daniel D'Avella <ddavella@stsci.edu>
2018-05-24 15:59:53 -0400

    Fix node name in write-compressed example

--- examples/write-compressed.cpp
+++ examples/write-compressed.cpp
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
         nums[i] = i;
     }
 
-    tree["array"] = Asdf::NDArray<int>(nums, array_size, CompressionType::zlib);
+    tree["data"] = Asdf::NDArray<int>(nums, array_size, CompressionType::zlib);
 
     std::ofstream outfile(argv[1]);
     outfile << asdf;
